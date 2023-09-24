5c1ea22346ad85f444b0c493f50ff8ef4087b0fe
bewisclient <137076694+bewisclient@users.noreply.github.com>
2023-09-24 10:30:37 +0200

    Add files via upload

--- scroll-bottom.js
+++ scroll-bottom.js
@@ -2,10 +2,10 @@ function click_bottom(bl) {
     const a = document.getElementById("scroll");
     var href = window.location.hash.substring(1);
     if(href=="bottom"==bl) {
-        a.style.rotate = "0deg";
+        a.style.rotate = "180deg";
         a.href = "#main";
     } else {
-        a.style.rotate = "180deg";
+        a.style.rotate = "0deg";
         a.href = "#bottom";
     }
 }
